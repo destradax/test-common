ada225623daf60c6f681d31fc3a0e4b683a34b95
destradax <destradax@gmail.com>
2020-08-19 22:29:08 -0500

    test

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
   api.cache(true);
   
   const presets = [ "@babel/preset-env", "@babel/preset-react" ];
-  const plugins = [ "macros" ];
+  const plugins = [ "macros", "@babel/plugin-transform-react-jsx" ];
 
   return {
     presets,
