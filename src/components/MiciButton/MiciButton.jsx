099367e0584417adb0d5e32d355ab5d7bbc414f0
Daniel Estrada <destradax@terminal.local>
2020-08-22 16:21:39 -0500

    test

---
+++ src/components/MiciButton/MiciButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './MiciButton.module.scss'
+
+const MiciButton = () => {
+  return <div className={styles.miciButton}>MiciButton</div>
+}
+
+export default MiciButton
